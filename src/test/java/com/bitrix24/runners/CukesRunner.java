9ab75d550afdd5a09b0576c0ee97018177bdd530
valentinadolontaeva <vdolontaeva@gmail.com>
2019-09-07 12:36:58 -0400

    new commit

--- src/test/java/com/bitrix24/runners/CukesRunner.java
+++ src/test/java/com/bitrix24/runners/CukesRunner.java
@@ -1,5 +1,4 @@
 package com.bitrix24.runners;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
@@ -7,27 +6,20 @@ import org.junit.runner.RunWith;
 
     @RunWith(Cucumber.class)
     @CucumberOptions(
-
             plugin = {
                     "json:target/cucumber.json",
                     "html:target/default-cucumber-reports"
-            },        
-      tags = {"@ScenarioOutlineAydin"},
-            features = {"src/test/resources/features"     } ,
-
+            },
+            features = {"src/test/resources/features/login/Login",
+            } ,
                        /*
                         * feature contains scenarios
                         * every scenario is like a test
                         * where is the implementation for features
                         */
-
             glue = {"com/bitrix24/step_definitions"},
-            dryRun = false
-
-
-
+            dryRun = true,
+            tags ="@wip"
     )
-
-
     public class CukesRunner {
     }
